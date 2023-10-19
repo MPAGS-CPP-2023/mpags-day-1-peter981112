ebc6a5fae2fc4e004156d961bcfa519786376399
Wonjong Chang <Wonjong.Chang@warwick.ac.uk>
2023-10-19 19:16:55 +0000

    arg and loop working

--- arg_test.cpp
+++ arg_test.cpp
@@ -5,10 +5,33 @@
 int main(int argc, char* argv[])
 { 
     int a{0};
-    a = argv.size()
-    for(int i=0; i<a; i++)
+    //std::cout<<"argc : "<<argc<<" argv 1 : "<<argv[1]<<std::endl;
+    const std::vector<std::string> cmdLineArgs { argv, argv+argc };
+    a = cmdLineArgs.size();
+    std::cout << "cmd line args : " << cmdLineArgs[1] <<std::endl;    
+    for(int i=1; i<a; i++)
     {
-        std::cout << argv[i] <<std::endl;
+        std::cout << i << " th arg is : " << cmdLineArgs[i] <<std::endl;
+        if(cmdLineArgs[i] == "-h" || cmdLineArgs[i] == "--help")
+        {
+            std::cout << "This is your help message. This program processes command line arguments" <<std::endl;
+        }
+        else if(cmdLineArgs[i] == "-v" || cmdLineArgs[i] == "--version")
+        {
+            std::cout << "version : 0.1  further version in development" <<std::endl;
+        }
+        else if(cmdLineArgs[i] == "-o" || cmdLineArgs[i] == "--output_file")
+        {
+            std::cout << "Loading output file......" <<std::endl;
+        }
+        else if(cmdLineArgs[i] == "-i" || cmdLineArgs[i] == "--input_file")
+        {
+            std::cout << "Loading input file......" <<std::endl;
+        }
+        else
+        {
+            std::cout << "Sorry but there is nothing to be done for option : "<< cmdLineArgs[i]<<" please try something else"<<std::endl;
+        }
     }
 
 
--- loop_test.cpp
+++ loop_test.cpp
@@ -6,73 +6,86 @@ int main()
     char temp_char;
     std::string out_str{""};
     int a{0};
-    int b{0};
+    int b{1};
  // Take each letter from user input and in each case:
     while(std::cin >> in_char)
     {
-        
+        std::cout<<in_char<<std::endl;
+        b=1;
         if(std::isalpha(in_char)==false)
         {
             //std::cout<<"Not alphbet!"<<std::endl;
             //break;
         
-        //std::cout<<std::toupper(in_char)<<std::endl;
-        
 
         //temp_char = toupper( in_char );
         //std::cout<<temp_char<<std::endl;
-        switch (in_char)
+            switch (in_char)
+            {
+                case '0':
+                    out_str += "ZERO";
+                    b = 4;
+                    break;
+                case '1':
+                    out_str += "ONE";
+                    b = 3;
+                    break;
+                case '2':
+                    out_str += "TWO";
+                    b = 3;
+                    break;
+                case '3':
+                    out_str += "THREE";
+                    b = 5;
+                    break;
+                case '4':
+                    out_str += "FOUR";
+                    b = 4;
+                    break;
+                case '5':
+                    out_str += "FIVE";
+                    b = 4;
+                    break;
+                case '6':
+                    out_str += "SIX";
+                    b = 3;
+                    break;
+                case '7':
+                    out_str += "SEVEN";
+                    b = 5;
+                    break;
+                case '8':
+                    out_str += "EIGHT";
+                    b = 5;
+                    break;
+                case '9':
+                    out_str += "NINE";
+                    b = 4;
+                    break;
+            
+            
+                default:
+                    // Do nothing
+                    break;
+            }
+        }
+        else out_str += std::toupper( in_char );
+        //std::cout<<"a : "<<a<<" str lst length : "<<out_str.size()<<std::endl;
+        //std::cout<<"out str : "<<out_str<<" a th compnent : "<<out_str[a]<<std::endl;
+        if(b==1)
         {
-            case '0':
-                out_str += "ZERO";
-                b = 4;
-                break;
-            case '1':
-                out_str += "ONE";
-                b = 3;
-                break;
-            case '2':
-                out_str += "TWO";
-                b = 4;
-                break;
-            case '3':
-                out_str += "THREE";
-                b = 5;
-                break;
-            case '4':
-                out_str += "FOUR";
-                b = 4;
-                break;
-            case '5':
-                out_str += "FIVE";
-                b = 4;
-                break;
-            case '6':
-                out_str += "SIX";
-                b = 3;
-                break;
-            case '7':
-                out_str += "SEVEN";
-                b = 5;
-                break;
-            case '8':
-                out_str += "EIGHT";
-                b = 5;
-                break;
-            case '9':
-                out_str += "NINE";
-                break;
-            
-            
-            default:
-                // Do nothing
-                break;
+            std::cout<<out_str[a]<<std::endl;
+            //std::cout<<out_str[a]<<std::endl;
         }
+        else
+        {
+            for(int i = 0; i < b; ++i)
+            {
+                std::cout<<out_str[a+i];
+            }
+            std::cout<<std::endl;
         }
-        out_str += std::toupper( in_char );
-
-        std::cout<<out_str[a]<<std::endl;
-        a++;
+        a = a + b;
         
         // - Convert to upper case
         // - Change numbers to words
